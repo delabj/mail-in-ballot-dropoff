16cbe1020efc9611f3b00f434ee894487a14a9c0
Arif Ali <arifyali@users.noreply.github.com>
2020-08-16 10:00:29 -0700

    Created PA file

--- scripts/cleaning/PA.R
+++ scripts/cleaning/PA.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(dplyr)
 library(readr)
-zip_code_database <- read_csv("Data/zip_code_database.csv")
+zip_code_database <- read_csv("Data/fcts/zip_code_database.csv")
 
 PA_zips <- zip_code_database %>% 
   filter(state == 'PA', type == 'STANDARD') %>%
@@ -11,6 +11,7 @@ PA_zips <- zip_code_database %>%
 # PA_zips[is.na(PA_zips$county),]
 ## Both Pittsburgh so mapping to the right countuy
 PA_zips$county[is.na(PA_zips$county)] = "Allegheny County"
-PA_county_electionoffice_map <- read_csv("Data/PA.csv")
+PA_county_electionoffice_map <- read_csv("Data/scraped/PA_election_offices_manual_collection.csv")
 
 PA_zip_to_election_office <- PA_zips %>% inner_join(PA_county_electionoffice_map)
+write_csv(PA_zip_to_election_office, "Data/drop-off-locations/PA.csv")
